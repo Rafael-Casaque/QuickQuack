cb76e44b8d946892d5e822afbc1aa7343b5970ec
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-09 20:11:55 -0300

    fix: created WebApp file, to run all class and controllers

---
+++ BackEnd/soarStream/src/main/java/com/soarStream/WebApp.java
@@ -0,0 +1,13 @@
+package com.soarStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
