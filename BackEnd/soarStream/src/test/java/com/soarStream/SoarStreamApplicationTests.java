0bc61fc6d448a5db70406de4077d6f7566f8dfef
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-09 20:03:12 -0300

    feat: create a spring boot base project

---
+++ BackEnd/soarStream/src/test/java/com/soarStream/SoarStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soarStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoarStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
