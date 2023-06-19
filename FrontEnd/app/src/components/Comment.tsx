9bc7dc1483fa28504e4bd6d299f82ba0cd388fb2
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-18 22:13:47 -0300

    feat: implemented void structures

--- FrontEnd/app/src/components/Comment.tsx
+++ FrontEnd/app/src/components/Comment.tsx
@@ -1,16 +1,24 @@
 import { Avatar, Flex, Text } from "@chakra-ui/react";
 
-export const Comment = () => {
+interface CommentProps {
+  author: string;
+  avatar: string;
+  content: string;
+}
+
+export const Comment = (props: CommentProps) => {
   return (
-    <Flex>
-      <Avatar />
+    <Flex direction="column" my="10px">
       <Flex>
-        <Text></Text>
-        <Text></Text>
+        <Avatar w="40px" h="40px" src={props.avatar}/>
+        <Flex direction="column" ml="10px" bg="#161616" borderRadius="10px" p="5px">
+          <Text fontWeight="bold">{props.author}</Text>
+          <Text align="justify">{props.content}</Text>
+        </Flex>
       </Flex>
-      <Flex>
+      <Flex align="center" ml="50px">
         <Text>Curtir</Text>
-        <Text>Responder</Text>
+        <Text mx="15px">Responder</Text>
         <Text>2h</Text>
       </Flex>
     </Flex>
