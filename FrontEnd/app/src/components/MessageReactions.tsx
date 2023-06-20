4056f364a433ff073e5016898640fb87ecfd808e
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-19 22:07:55 -0300

    feat: implemented portals and states

--- FrontEnd/app/src/components/MessageReactions.tsx
+++ FrontEnd/app/src/components/MessageReactions.tsx
@@ -1,9 +1,18 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { LikeIcon } from "../icons/LikeIcon";
 import { CommentIcon } from "../icons/CommentIcon";
 import { ShareIcon } from "../icons/ShareIcon";
+import { useState } from "react";
+import { ShareModal } from "./Modals/ShareModal";
+import { ReplyPostModal } from "./Modals/ReplyPostModal";
+import { CustomModal } from "./Modals/CustomModal";
+import { SelectedLikeIcon } from "../icons/SelectedLikeIcon";
 
 export const MessageReactions = () => {
+  const [modal, setModal] = useState<JSX.Element>(<></>);
+  const [like, setLike] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex
       bg="black"
@@ -13,24 +22,61 @@ export const MessageReactions = () => {
       align="center"
       justify="space-around"
     >
-      <Flex align="center" justify="center" css={{ cursor: "pointer" }}>
-        <LikeIcon fill="white" boxSize="20px" />
+      <Flex align="center" justify="center" css={{ cursor: "pointer" }} onClick={()=>{setLike(!like)}}>
+        {like ? (
+          <LikeIcon fill="white" boxSize="20px" />
+        ) : (
+          <SelectedLikeIcon fill="white" boxSize="20px" />
+        )}
         <Text ml="5px" color="white">
           20
         </Text>
       </Flex>
-      <Flex align="center" justify="center" css={{ cursor: "pointer" }}>
+      <Flex
+        id="comment"
+        align="center"
+        justify="center"
+        css={{ cursor: "pointer" }}
+        onClick={(e) => {
+          changeModal(e.currentTarget.id, setModal, onOpen);
+        }}
+      >
         <CommentIcon fill="white" boxSize="30px" />
         <Text ml="5px" color="white">
           20
         </Text>
       </Flex>
-      <Flex align="center" justify="center" css={{ cursor: "pointer" }}>
+      <Flex
+        id="share"
+        align="center"
+        justify="center"
+        css={{ cursor: "pointer" }}
+        onClick={(e) => {
+          changeModal(e.currentTarget.id, setModal, onOpen);
+        }}
+      >
         <ShareIcon fill="white" boxSize="22px" />
         <Text ml="5px" color="white">
           20
         </Text>
       </Flex>
+      <CustomModal isOpen={isOpen} onClose={onClose} children={modal} />
     </Flex>
   );
+};
+
+const changeModal = (
+  id: string,
+  setModal: React.Dispatch<React.SetStateAction<JSX.Element>>,
+  onOpen: () => void
+) => {
+  onOpen();
+  switch (id) {
+    case "share":
+      setModal(<ShareModal btnFunction={() => {}} />);
+      break;
+    case "comment":
+      setModal(<ReplyPostModal btnFunction={() => {}} />);
+      break;
+  }
 };
