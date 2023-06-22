425eeceb5b2057a1c762e732fff15dd98adc451c
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-22 19:36:10 -0300

    Fix: restored test component

--- FrontEnd/app/src/App.tsx
+++ FrontEnd/app/src/App.tsx
@@ -1,9 +1,3 @@
-import { AccessPage } from "./pages/AccessPage";
-import { FeedPage } from "./pages/FeedPage";
-import { ErrorPage } from "./pages/ErrorPage";
-
 export const App = () => {
-  return (
-    <ErrorPage></ErrorPage>
-  );
+  return <></>;
 };
