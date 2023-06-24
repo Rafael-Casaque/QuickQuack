9facd60f956c405e8bb29f03ef6e269106dea297
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-24 11:36:18 -0300

    feat: created conditional renderization by mediaType

--- FrontEnd/app/src/pages/FeedPage.tsx
+++ FrontEnd/app/src/pages/FeedPage.tsx
@@ -10,11 +10,11 @@ export const FeedPage = () => {
       <Flex ml={{ base: "60px", md: "60px", lg: "325px" }} w={{base:"calc(100% - 60px)",md:"calc(100% - 60px - 250px)",lg:"calc(100% - 650px)"}} minH="100%" direction="column" align="center">        
       <Text alignSelf="flex-start" m="20px" fontSize="30px" fontWeight="bold">Home</Text>
       <Divider borderColor="black" mb="10px"/>
-        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]}/>
-        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]}/>
-        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]}/>        
-        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]}/>        
-        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]}/>        
+        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]} media={"https://umnovocaomeco.web.app/imagens/dogs/Dogs2/2.jpg"}/>
+        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]} media={"https://umnovocaomeco.web.app/imagens/dogs/Dogs2/2.jpg"}/>
+        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]} media={"https://umnovocaomeco.web.app/imagens/dogs/Dogs2/2.jpg"}/>        
+        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]} media={"https://umnovocaomeco.web.app/imagens/dogs/Dogs2/2.jpg"}/>        
+        <Message createdBy={""} usernameAuthor={""} createdAt={new Date()} description={""} CommentList={[]} likeList={[]} shareList={[]} media={"https://umnovocaomeco.web.app/imagens/dogs/Dogs2/2.jpg"}/>        
       </Flex>
       <TrendingMessages trendingMessagesList={['','','','','']}/>
     </Flex>
