fc1efd386d364a301e18f8e5080c88dbbfed1fde
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-06-23 22:18:43 -0300

    feat: implemented responsive styles at notFoundErrorPagee

--- FrontEnd/app/src/pages/NotFoundPage.tsx
+++ FrontEnd/app/src/pages/NotFoundPage.tsx
@@ -2,32 +2,30 @@ import { Flex } from "@chakra-ui/react";
 import { ErrorMessage } from "../components/ErrorMessage";
 
 export const NotFoundPage = () => {
-    return (
-      <>
+  return (
+    <>
+      <Flex
+        style={{ perspective: "1000px" }}
+        minH="100vh"
+        w="100%"
+        align="center"
+        justify="center"
+        bg="black"
+      >
         <Flex
-          style={{ perspective: "1000px" }}
-          minH="100vh"
-          w="100%"
-          align="center"
-          justify="center"
-          bg="black"
-        >
-          <Flex
-          style={{ perspective: "1000px" }}
-          maxH="80vh"
-          h="600px"
-          maxW="80%"
-          w="800px"
+          style={{ perspective: "1000px" }}          
+          h="500px"          
+          w="95%"
+          maxW="800px"
           align="center"
           justify="center"
           direction="column"
           backgroundImage="linear-gradient(to left top, #2a32e7, #0052f2, #006bf9, #0081fc, #2496fc, #4f98fc, #699afc, #7e9cfc, #9f89f5, #c171e4, #df52c8, #f423a2)"
           borderRadius="50px"
         >
-          <ErrorMessage></ErrorMessage>
+          <ErrorMessage />
         </Flex>
-        </Flex>
-      </>
-    );
-  };
-  
+      </Flex>
+    </>
+  );
+};
